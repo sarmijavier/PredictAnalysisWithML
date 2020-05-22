f73af259157495bc9f3ad848b8534cd3cf4ead4f
Javier Sarmiento <mayayahuarcani@gmail.com>
2020-05-22 01:25:17 -0500

    ver 2.0

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import ImageDetail from '@/components/ImageDetail'
 
 Vue.use(Router)
 
 const router = new Router({
     mode: 'history',
     routes: [{
-        path: '/',
-        name: 'Home',
-        component: Home
-    }]
+            path: '/',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path: '/image/:id',
+            component: ImageDetail
+        }
+    ]
 
 })
 
